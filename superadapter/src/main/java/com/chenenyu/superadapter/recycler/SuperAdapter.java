f284fe0e1713bbd7260285bab6b5763493d23c62
chenenyu <echocey@gmail.com>
2016-01-15 14:14:27 +0800

    bug fixed.

--- superadapter/src/main/java/com/chenenyu/superadapter/recycler/SuperAdapter.java
+++ superadapter/src/main/java/com/chenenyu/superadapter/recycler/SuperAdapter.java
@@ -3,6 +3,7 @@ package com.chenenyu.superadapter.recycler;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import java.util.List;
 
@@ -48,14 +49,16 @@ public abstract class SuperAdapter<T> extends BaseSuperAdapter<T, BaseViewHolder
             holder = new BaseViewHolder(mLayoutInflater.inflate(mLayoutResId, parent, false));
         }
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mOnItemClickListener != null) {
-                    mOnItemClickListener.onItemClick(v, viewType, holder.getLayoutPosition());
+        if (!(holder.itemView instanceof AdapterView)) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mOnItemClickListener != null) {
+                        mOnItemClickListener.onItemClick(v, viewType, holder.getLayoutPosition());
+                    }
                 }
-            }
-        });
+            });
+        }
         return holder;
     }
 }
